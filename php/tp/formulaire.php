3879e5c693bffd72e1e46be52008e68a0b86c896
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-03-30 16:39:27 +0200

    add

---
+++ php/tp/formulaire.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tp Nasa</title>
+</head>
+
+<body>
+    <form action="secret.php" method="POST">
+        <p><label>Mot de pass<input type="text" name="mot_de_passe"></label></p>
+        <p><input type="submit"></p>
+
+    </form>
+</body>
+
+</html>
